9d33ca1cec6e11f17ddd97ed552ef6abf71a24cb
Adhil03 <adlpro253@gmail.com>
2026-01-02 11:16:36 +0530

    Add TodoItem model, views, and template for managing todo items

--- todo/myapp/views.py
+++ todo/myapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from .models import TodoItem
 
 # Create your views here.
 
@@ -6,3 +7,8 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+
+def todos(request):
+    items = TodoItem.objects.all()
+    return render(request, "todos.html", {"todos": items})
