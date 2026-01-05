22b9410c0c22e8a478e665930f9616eecf920ecc
Adhil03 <adlpro253@gmail.com>
2026-01-05 11:16:35 +0530

    Implement CRUD functionality for Todo items with toggle and delete options

--- todo/myapp/views.py
+++ todo/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from .models import TodoItem
 
 
@@ -8,5 +8,21 @@ def home(request):
 
 
 def todos(request):
+    if request.method == "POST":
+        new_todo = request.POST.get("new_todo")
+        TodoItem.objects.create(title=new_todo)
     items = TodoItem.objects.all()
     return render(request, "todos.html", {"todos": items})
+
+
+def toggle_todo(request, todo_id):
+    todo = TodoItem.objects.get(id=todo_id)
+    todo.completed = not todo.completed
+    todo.save()
+    return redirect("/todos")
+
+
+def delete_todo(request, todo_id):
+    todo = TodoItem.objects.get(id=todo_id)
+    todo.delete()
+    return redirect("/todos")
